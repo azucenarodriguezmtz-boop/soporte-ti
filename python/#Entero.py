3f61dacd6bef396fcbf5d1c9706ee00d48a345c1
azucenarodriguezmtz-boop <azucena.rodriguez.mtz@gmail.com>
2025-12-16 12:22:08 -0600

    Add new files and directories

---
+++ python/#Entero.py
@@ -0,0 +1,9 @@
+#Entero
+5, -3, 0, 42
+
+#Flotante
+3.14, -0.001, 2.0
+
+#STR
+print ("5" + "veces" + "!")
+
---
+++ python/main.py
@@ -0,0 +1,18 @@
+#Definicion de variables
+nombre = "Azucena"
+edad = 32
+altura = 1.60
+
+print("Hola soy:",nombre)
+print("y tengo:",edad, "y mido:",altura)
+print("el proximo año cumplo", edad + 1)
+
+precio_producto =19.99
+cantidad = 3
+total = precio_producto * cantidad
+print("El total a pagar es:", total)
+
+mayor_edad = True
+tiene_ beca = False
+if mayor_edad: 
+    print("Puede solicitar beca")
